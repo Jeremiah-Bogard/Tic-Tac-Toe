1cf259ea414fbddb50e034b27635667deec45dde
Jeremiah <96797719+Jeremiah-Bogard@users.noreply.github.com>
2022-01-23 16:14:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,117 +1,81 @@
-const tiles = Array.from(document.querySelectorAll(".tile"));
-const playerDisplay = document.querySelector(".display-player");
-const resetButton = document.querySelector("#reset");
-const announcer = document.querySelector(".announcer");
-  
-let board = ["", "", "", "", "", "", "", "", "",];
-let currentPlayer = "X";
-let isGameActive = true;
-  
-const PLAYERX_WON = "PLAYERX_WON";
-const PLAYERO_WON = "PLAYERO_WON";
-const TIE = "TIE";
-  
-const winningConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 3, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
-  
-function handleResultValidation() {
-  let roundWon = false;
-  for(let i = 0; i <= 7; i++) {
-    const winCondition = winningConditions[i];
-    const a = board[winCondition[0]];
-    const b = board[winCondition[1]];
-    const c = board[winCondition[2]];
-     
-    if(a === "" || b === "" || c === "") {
-      continue;
-    }
-    if(a === b && b === c) {
-      roundWon = true;
-      break;
-    }
-  }
-   
-  if(roundWon) {
-    announce(currentPlayer === "X" ? PLAYERX_WON : PLAYERO_WON);
-    isGameActive = false;
-    return;
-  }
-  if(!board.includes("")) {
-    announce(TIE);
-  }
-}
-    
-  
-const announce = (type) => {
-  switch(type) {
-    case PLAYERO_WON:
-      announcer.innerHTML = "Player <span class='playerO'>O</span> Won";
-      break;
-    case PLAYERX_WON:
-      announcer.innerHTML = "Player <span class='playerX'>X</span> Won";
-      break;
-    case TIE:
-      announcer.innerText = "Tie";
-  }
-  announcer.classList.remove("hide");
-};
+var currentPlayer = "X";
 
-const isValidAction = (tile) => {
-  if(tile.innerText === "X" || tile.innerText === "O") {
-    return false;
-  }
+var cards = document.getElementsByClassName("card");
+
+var waysToWin = [
+  [1, 2, 3],
+  [1, 5, 9],
+  [1, 4, 7],
+  [4, 5, 6],
+  [2, 5, 8],
+  [3, 5, 7]
+];
+
+function checkForWinner() {
+  
+  for(var i = 0; i < waysToWin.length; i++) {
     
-  return true;
-};
+    var index = waysToWin[i];
+      
+    var card1 = cards[index[0]-1].textContent;
+    var card2 = cards[index[1]-1].textContent;
+    var card3 = cards[index[2]-1].textContent;
+      
+    if(card1 === card2 && card2 === card3 && card1 !== "" && card2 !== "" && card3 !== "") {
+      var winnerIs = card1 || card2 || card3;
+      
+      alert("Player " + winnerIs + " Wins!!!!");
+    }
+
+  }
   
-const updateBoard = (index) => {
-  board[index] = currentPlayer;
 }
+
+function updatePlayer() {
   
-const changePlayer = () => {
-  palyerDisplay.classList.remove("player${currentPlayer}");
   currentPlayer = currentPlayer === "X" ? "O" : "X";
-  playerDisplay.innerText = currentPlayer;
-  playerDisplay.classList.add("player${currentPlayer}");
+  
+  document.getElementById("playerTurn").innerText = currentPlayer;
+  
 }
+
+function updateCard(card) {
   
-const userAction = (tile, index) => {
-  if(isValidAction(tile) && isGameActive) {
-    tile.innerText = currentPlayer;
-    tile.classList.add("player${currentPlayer}");
-    updateBoard(index);
-    handleResultValidation();
-    changePlayer();
-  }
-}
-  
-const resetBoard = () => {
-  board = ["", "", "", "", "", "", "", "", ""];
-  isGameActive = true;
-  announcer.classList.add("hide");
-   
-  if(currentPlayer === "O") {
-    changePlayer();
-  }
-   
-  tiles.forEach(tile => {
-    tile.innerText = "";
-    tile.classList.remove("playerX");
-    tile.classList.remove("playerO");
-  });
-};
-  
-tiles.forEach((tile, index) => {
-  tile.addEventListener("click", userAction(tile, index));
-});
+  if(card.innerText === "") {
     
+    card.innerText = currentPlayer;
+    updatePlayer();
+    checkForWinner();
+    return;
+    
+  }
   
-resetButton.addEventListener("click", resetBoard);
+  alert("Incorrect Move\nPlease try a different spot")
+  
+}
+
+for(var i = 0; i < cards.length; i++) {
+  
+  cards[i].addEventListener("click", function() {
+    updateCard(this);
+  });
+  
+}
+
+function reset() {
+  
+  for(var i = 0; i < cards.length; i++) {
+    
+    cards[i].innerText = "";
+    
+  }
+  
+  if(currentPlayer === "O") {
+    updatePlayer();
+  }
+  
+}
+
+var resetBttn = document.getElementById("reset");
+
+resetBttn.addEventListener("click", reset);
